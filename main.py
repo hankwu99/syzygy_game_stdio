7647b1f065b330c20515ce2f40baa5301e6c2bf0
hankwu99 <47174225+hankwu99@users.noreply.github.com>
2020-01-24 14:26:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
 import sys
 import pygame
 from pygame.locals import QUIT
+import tkinter as tk
 
-# 初始化
-pygame.init()
 
-window_surface = pygame.display.set_mode((800, 1000))
-pygame.display.set_caption('Syzygy Music')
-window_surface.fill((0, 0, 0))
+def game():
+    pygame.init() # 初始化
 
-# 宣告 font 文字物件
-head_font = pygame.font.SysFont(None, 60)
-# 渲染方法會回傳 surface 物件
-text_surface = head_font.render('                            Syzygy', True, (255, 255, 255))
-# blit 用來把其他元素渲染到另外一個 surface 上，這邊是 window 視窗
-window_surface.blit(text_surface, (10, 10))
+    window_surface = pygame.display.set_mode((800, 1000))
+    pygame.display.set_caption('Syzygy Music')
+    window_surface.fill((0, 0, 0))
+   
+    pygame.display.update()  # 更新畫面，等所有操作完成後一次更新（若沒更新，則元素不會出現）
 
-# 更新畫面，等所有操作完成後一次更新（若沒更新，則元素不會出現）
-pygame.display.update()
+    while True:  # 視窗關閉設定
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
 
-# 視窗關閉設定
-while True:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
+window=tk.Tk()
+window.title('Syzygy Music')
+window.geometry('800x1000')
+window.configure(background='black')
+
+title_label = tk.Label(window, text = 'Syzygy Music', font = ('Kaiso-Next-B', 50), width = 70, height = 6)
+title_label.pack()
+
+button_start = tk.Button(window, text = "開始遊戲", font = ('Noto Serif CJK TC', 20), width = 10, height = 2, command = game)
+button_start.pack()
+
+window.mainloop()
