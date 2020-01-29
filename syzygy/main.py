aa1dd96c7a868fd8a7fcba0fb65794d4936a9244
hankwu99 <47174225+hankwu99@users.noreply.github.com>
2020-01-29 11:27:31 +0800

    Add files via upload

--- syzygy/main.py
+++ syzygy/main.py
@@ -9,12 +9,12 @@ import music_2
 #------------------------------------------------------Pygame area code---------------------------------------------------------#
 
 def game_music1():
-    window.destroy()
+    background.destroy() 
     pygame.mixer.music.stop()
     music_1.play() # customize module
 
 def game_music2():
-    window.destroy()
+    background.destroy()
     pygame.mixer.music.stop()
     music_2.play() # customize module
 
@@ -23,31 +23,31 @@ def game_music2():
 #------------------------------------------------------Music menu code----------------------------------------------------------#
 def music1():
     pygame.mixer.music.stop()
-    pygame.mixer.music.load('riot.mp3')
+    pygame.mixer.music.load('Brain Power.mp3')
     pygame.mixer.music.set_volume(0.5)
     pygame.mixer.music.play()
 
 def music2():
     pygame.mixer.music.stop()
-    pygame.mixer.music.load('KVDS-Move.mp3')
+    pygame.mixer.music.load('打打打打打打打打打打.mp3')
     pygame.mixer.music.set_volume(0.5)
     pygame.mixer.music.play()
 
 def music_menu():
-    menu = tk.Toplevel(window)
+    menu = tk.Toplevel(background)
     menu.title('Music Menu')
-    menu.geometry('900x1000')
+    menu.geometry('900x1014')
     menu.resizable(False, False)
 
     label = tk.Label(menu, text = 'Music Menu', font = ('Agency FB', 30), width = 70, height = 3)
 
-    music1_name = tk.Label(menu, text = 'Music_1\nR・I・O・T by Raise a Suilen', font = ('Agency FB', 20), width = 70, height = 2)
+    music1_name = tk.Label(menu, text = 'Music_1\nNOMA - Brain Power', font = ('Agency FB', 20), width = 70, height = 2)
     music1_play = tk.Button(menu, text = '試聽一段', font = ('Agency FB', 15), width = 15, height = 1, command = music1)
     music1_game = tk.Button(menu, text = 'play this', font = ('Agency FB', 20), width = 10, height = 1, command = game_music1)
 
     ___label = tk.Label(menu, font = ('Agency FB', 10), width = 50, height = 2)
 
-    music2_name = tk.Label(menu, text = 'Music_2\nKVDS - Move', font = ('Agency FB', 20), width = 70, height = 2)
+    music2_name = tk.Label(menu, text = 'Music_2\nHige Driver join. SELEN - 打打打打打打打打打打', font = ('Agency FB', 20), width = 70, height = 2)
     music2_play = tk.Button(menu, text = '試聽一段', font = ('Agency FB', 15), width = 15, height = 1, command = music2)
     music2_game = tk.Button(menu, text = 'play this', font = ('Agency FB', 20), width = 10, height = 1, command = game_music2)
 
@@ -68,9 +68,9 @@ def music_menu():
 #------------------------------------------------------information code---------------------------------------------------------#
 
 def info():
-    info = tk.Toplevel(window)
+    info = tk.Toplevel(background)
     info.title('More Information')
-    info.geometry('700x700')
+    info.geometry('900x600')
     info.resizable(False, False)
 
     label = tk.Label(info, text = 'More Information', font = ('Agency FB', 30), width = 70, height = 3)
@@ -89,6 +89,12 @@ def info():
 
 #-------------------------------------------------------Main menu code----------------------------------------------------------#
 
+background=tk.Tk()
+background.title('Music')
+background.geometry('900x1014')
+background.configure(background='black')
+background.resizable(False, False)
+
 pygame.mixer.init()
 pygame.mixer.music.set_volume(0.7) 
 
@@ -96,29 +102,22 @@ if not pygame.mixer.music.get_busy():
     pygame.mixer.music.load('Inspirational.mp3')
     pygame.mixer.music.play()
 
-window=tk.Tk()
-window.title('Music')
-window.geometry('900x1000')
-window.configure(background='black')
-window.resizable(False, False)
+title_label = tk.Label(background, text = 'Music Game', font = ('Agency FB', 50), width = 50, height = 4)
 
-title_label = tk.Label(window, text = 'Music Game', font = ('Agency FB', 50), width = 50, height = 4)
+label = tk.Label(background, bg = "black", font = ('Agency FB', 40), width = 50, height = 1)
+button_start = tk.Button(background, text = "Start", font = ('Agency FB', 20), width = 15, height = 2, command = music_menu)
 
-label = tk.Label(window, bg = "black", font = ('Agency FB', 40), width = 50, height = 1)
-button_start = tk.Button(window, text = "Start", font = ('Agency FB', 20), width = 15, height = 2, command = music_menu)
+_label = tk.Label(background, bg = "black", font = ('Agency FB', 40), width = 50, height = 1)
+button_info = tk.Button(background, text = "More Info.", font = ("Agency FB", 20), width = 15, height = 2, command = info)
 
-_label = tk.Label(window, bg = "black", font = ('Agency FB', 40), width = 50, height = 1)
-button_info = tk.Button(window, text = "More Info.", font = ("Agency FB", 20), width = 15, height = 2, command = info)
+__label = tk.Label(background, bg = "black", font = ('Agency FB', 40), width = 50, height = 1)
+button_quit = tk.Button(background, text = "Quit", font = ('Agency FB', 20), width = 15, height = 2, command = background.destroy)
 
-__label = tk.Label(window, bg = "black", font = ('Agency FB', 40), width = 50, height = 1)
-button_quit = tk.Button(window, text = "Quit", font = ('Agency FB', 20), width = 15, height = 2, command = window.destroy)
-
-version = tk.Label(window, bg = "black", fg= "white", text = 'Beta v 0.0.0', font = ("Agency FB", 18), width = 1000, height = 1)
+version = tk.Label(background, bg = "black", fg= "white", text = 'Beta v 0.0.0', font = ("Agency FB", 18), width = 1000, height = 1)
 version.pack(side='bottom')
 
-author_label = tk.Label(window, text = 'Made By Syzygy Games Studio', font = ('Agency FB', 18), width = 1000, height = 3)
+author_label = tk.Label(background, text = 'Made By Syzygy Games Studio', font = ('Agency FB', 18), width = 1000, height = 3)
 author_label.pack(side='bottom')
-
 
 title_label.pack()
 
@@ -135,6 +134,6 @@ author_label.pack()
 
 version.pack()
 
-window.mainloop()
+background.mainloop()
 
 #-------------------------------------------------------Main menu code----------------------------------------------------------#
--- syzygy/music_1.py
+++ syzygy/music_1.py
@@ -1,35 +1,48 @@
 import sys
-import pygame
+import pygame as pg
 from pygame.locals import QUIT
 from pygame.color import THECOLORS
 
 def play():
-    pygame.init() # pygame初始化
+    pg.init()
 
-    window = pygame.display.set_mode((1400, 1080)) # pygame screen size
-    windowcaption = pygame.display.set_caption('Syzygy Music') #screen name
+    #main screen setting
+    window = pg.display.set_mode((1400, 1014)) 
+    windowcaption = pg.display.set_caption('Syzygy Music') 
     window.fill((0, 0, 0))
-
-    background = pygame.Surface(window.get_size()) #背景畫布
+    background = pg.Surface(window.get_size())
     background.convert()
     background.fill((0, 0, 0))
     window.blit(background,(0, 0)) 
 
-    pygame.draw.rect(window, THECOLORS['white'], [340, 0, 120 , 1080], 1)
-    pygame.draw.rect(window, THECOLORS['white'], [460, 0, 120 , 1080], 1)
-    pygame.draw.rect(window, THECOLORS['white'], [580, 0, 120 , 1080], 1)
-    pygame.draw.rect(window, THECOLORS['white'], [700, 0, 120 , 1080], 1)
-    pygame.draw.rect(window, THECOLORS['white'], [820, 0, 120 , 1080], 1)
-    pygame.draw.rect(window, THECOLORS['white'], [940, 0, 120 , 1080], 1)
+    #music name
+    game_name = pg.font.SysFont('Agency FB', 40)
+    text = game_name.render("NOMA - Brain Power", True, THECOLORS['white'])
+    background.blit(text, [10, 5])
 
-    pygame.draw.rect(window, THECOLORS['white'], [340, 540, 720 , 5], 0)
+    #music play 
+    pg.mixer.init()
+    pg.mixer.music.set_volume(0.7) 
+    pg.mixer.music.load('Brain Power.mp3')
+    pg.mixer.music.play()
 
+    #key painting
+    pg.draw.rect(background, THECOLORS['white'], [340, 0, 120 , 1014], 1)
+    pg.draw.rect(background, THECOLORS['white'], [460, 0, 120 , 1014], 1)
+    pg.draw.rect(background, THECOLORS['white'], [580, 0, 120 , 1014], 1)
+    pg.draw.rect(background, THECOLORS['white'], [700, 0, 120 , 1014], 1)
+    pg.draw.rect(background, THECOLORS['white'], [820, 0, 120 , 1014], 1)
+    pg.draw.rect(background, THECOLORS['white'], [940, 0, 120 , 1014], 1)
+    pg.draw.line(background, [255, 255, 255], [340, 507], [1059, 507], 5)
 
+    #FPS
+    fps_clock = pg.time.Clock() 
 
-
-    pygame.display.update() 
-
-    while True:  # 視窗關閉設定
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                sys.exit()
+    running = True
+    while running:  # 視窗關閉設定
+        fps_clock.tick(50)
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                runing = False
+        window.blit(background, (0, 0))
+        pg.display.update() 
